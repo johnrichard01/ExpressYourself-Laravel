5d881f992b45978530e3533bf9d26efabd127249
johnrichard01 <pro.richardsumagui001@gmail.com>
2024-01-25 14:45:21 +0800

    done login signup and redirect

--- app/Http/Controllers/BlogsController.php
+++ app/Http/Controllers/BlogsController.php
@@ -4,12 +4,27 @@ namespace App\Http\Controllers;
 
 use App\Models\Blogs;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class BlogsController extends Controller
 {
     public function index(){
         $latestBlog = Blogs::latest()->first();
         $blogs=Blogs::where('id', '!=', $latestBlog->id)->latest()->simplePaginate(4);
-        return view('homepage.index', compact('blogs','latestBlog'));
+        if(Auth::check())
+        {
+            if(Auth::user()->role_as == '1')
+            {
+                return redirect('/dashboard');
+            }
+            else if(Auth::user()->role_as == '0')
+            {
+                return view('homepage.index', compact('blogs','latestBlog'));
+            }
+        }
+        else
+        {
+            return view('homepage.index', compact('blogs','latestBlog'));
+        }
     }
 }
--- app/Http/Controllers/StaticPageController.php
+++ app/Http/Controllers/StaticPageController.php
@@ -3,10 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class StaticPageController extends Controller
 {
     public function aboutus(){
-        return view('homepage.aboutus');
+        if(Auth::check())
+        {
+            if(Auth::user()->role_as == '1')
+            {
+                return redirect('/dashboard');
+            }
+            else if(Auth::user()->role_as == '0')
+            {
+                return view('homepage.aboutus');
+            }
+        }
+        else
+        {
+            return view('homepage.aboutus');
+        }
+        
     }
 }
