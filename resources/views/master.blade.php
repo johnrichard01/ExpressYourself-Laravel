12cc8dd1265be936102b9ca8316792c5337c3878
johnrichard01 <pro.richardsumagui001@gmail.com>
2024-03-25 20:22:16 +0800

    done

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>@yield('title')</title>
-    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.css')}}">
+    <link rel="stylesheet" href="https://expressyourself-laravel-production.up.railway.app/assets/css/bootstrap.css">
     <script src="//unpkg.com/alpinejs" defer></script>
     <link rel="icon" type="image/x-icon" href="https://expressyourself-laravel-production.up.railway.app/assets/images/favicon.png">
     @yield('css')
